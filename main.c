19e12def137062dcf78267f162501c9818a35885
jcaf <jcaf@archlinux>
2025-04-18 21:13:23 -0500

    Adding EEPROM 4

--- main.c
+++ main.c
@@ -52,8 +52,8 @@
 #include "adc/adc.h"
 #include "main.h"
 
-uint16_t EEMEM PWM = 0;
-uint16_t EEMEM ICR1_latest = 0;
+uint16_t EEMEM OCR1A_EEMEM = 0;
+uint16_t EEMEM ICR1_EEMEM = 0;
 
 #define VOUT_MAX 10.0f         //Volts
 
@@ -68,9 +68,8 @@ uint16_t EEMEM ICR1_latest = 0;
 #define EQUATION_solve_b(y,m,x) (y-(m*x))
 #define EQUATION_b EQUATION_solve_b(PWM_BIT_RESOL_MIN,EQUATION_m,CALIBSTEP_ADC_VALUE_MIN)
 
-uint8_t PWM_BIT_RESOL_SELECTED;
-uint8_t PWM_BIT_RESOL_SELECTED_latest;
-uint8_t EEMEM PWM_BIT_RESOL_SELECTED_EEMEM_latest = 0;
+uint8_t PWM_RESOL_SELECTED;
+uint8_t EEMEM PWM_RESOL_SELECTED_EEMEM = 0;
 
 float voltage_step;
 
@@ -102,46 +101,6 @@ void ADC_config2ReadPotSetVoltStep(void)
     ADC_setBit_startConversion_On();
 }
 
-
-//void readPotSetVoltStep(void)
-
-int8_t updateIfChange(uint8_t *pwm_bit_resol_selected_latest)
-{  
-    volatile uint8_t adclow = ADCL;//Captura la conversion actual, pero uso ADCH
-    PWM_BIT_RESOL_SELECTED =  (uint8_t) ((ADCH*EQUATION_m) +  EQUATION_b);
-    
-    if (*pwm_bit_resol_selected_latest != PWM_BIT_RESOL_SELECTED)
-    {
-        
-        print_pwm_bit_resol(PWM_BIT_RESOL_SELECTED);
-        //
-        voltage_step = (float)(VOUT_MAX/((uint32_t)0x00001<<PWM_BIT_RESOL_SELECTED));
-        print_voltageStep(voltage_step);
-        
-        ConfigInputPin(DDRB, 1); //Disable output pin
-        ICR1 = (uint16_t) (((uint32_t)0x00001<<PWM_BIT_RESOL_SELECTED) -1);
-        if ((*pwm_bit_resol_selected_latest > 0) && (OCR1A>0))
-        {
-            //factor = 2^x/ OCR actual ;
-            //OCR = 2^new/factor;//Valor actual de OCR
-            float factor = (1<<*pwm_bit_resol_selected_latest) / OCR1A;//antiguo
-            OCR1A = (int) ((1<<PWM_BIT_RESOL_SELECTED)/factor);//nuevo
-        }
-        else
-        {
-            OCR1A = 0;
-        }
-        eeprom_update_word(&PWM, OCR1A);
-        eeprom_update_word(&ICR1_latest, ICR1);
-        ConfigOutputPin(DDRB, 1); //Enable output pin
-        //
-        //Al ultimo recien actualiza el valor
-        *pwm_bit_resol_selected_latest = PWM_BIT_RESOL_SELECTED;
-        eeprom_update_byte(&PWM_BIT_RESOL_SELECTED_EEMEM_latest, *pwm_bit_resol_selected_latest);
-        return 1;
-    }
-    return 0;
-}    
 void print_vout(float vout)
 {
     char buff[20];
@@ -168,50 +127,93 @@ void print_pwm_bit_resol(uint8_t PWM_BIT_RESOL_SELECTED)
     lcdan_print_string(buff);
     lcdan_print_string("-BIT ");
 }
+uint8_t get_pwm_bitresol(void)
+{
+    uint8_t adclow = ADCL;//Captura la conversion actual, pero uso ADCH
+    return  (uint8_t) ((ADCH*EQUATION_m) +  EQUATION_b);
+}
+
+int8_t updatePWMRegsIfChange(uint8_t PWM_RESOL_SELECTED)
+{  
+    uint8_t PWM_RESOL_SELECTED_latest = eeprom_read_byte(&PWM_RESOL_SELECTED_EEMEM);
+    if (PWM_RESOL_SELECTED_latest != PWM_RESOL_SELECTED)
+    {
+        //Disable output pin
+        ConfigInputPin(DDRB, 1); 
+        ICR1 = (uint16_t) (((uint32_t)0x00001<<PWM_RESOL_SELECTED) -1);
+        if ((PWM_RESOL_SELECTED_latest > 0) && (OCR1A>0))
+        {
+            //factor = 2^x/ OCR actual ;
+            //OCR = 2^new/factor;//Valor actual de OCR
+            float factor = (1<<PWM_RESOL_SELECTED_latest) / OCR1A;//antiguo
+            OCR1A = (int) ((1<<PWM_RESOL_SELECTED)/factor);//nuevo
+        }
+        else
+        {
+            OCR1A = 0;
+        }
+        eeprom_update_word(&OCR1A_EEMEM, OCR1A);
+        eeprom_update_word(&ICR1_EEMEM, ICR1);
+        ConfigOutputPin(DDRB, 1); //Enable output pin
+        //
+        //Al ultimo recien actualiza el valor
+        eeprom_update_byte(&PWM_RESOL_SELECTED_EEMEM, PWM_RESOL_SELECTED);
+        //
+        print_pwm_bit_resol(PWM_RESOL_SELECTED);
+        //
+        voltage_step = (float)(VOUT_MAX/((uint32_t)0x00001<<PWM_RESOL_SELECTED));
+        print_voltageStep(voltage_step);
+
+        return 1;
+    }
+    return 0;
+}    
+
+
 int main(void)
 {
     int counter0 = 0;
     int counter1 = 0;
-
+    
     ADC_config2ReadPotSetVoltStep();
     
     lcdan_init();
     ConfigOutputPin(CONFIGIOxLCDAN_BK, PINxLCDAN_BK);
     PinTo1(PORTWxLCDAN_BK, PINxLCDAN_BK);
     
-    //        
     //Config to 1ms
     TCNT0 = 0x00;
     TCCR0A = (1 << WGM01);
     TCCR0B = (0 << CS02) | (1 << CS01) | (1 << CS00); //CTC PRES = 64
     OCR0A = CTC_SET_OCR_BYTIME(1e-3, 64); // 
-    //
     TIMSK0 |= (1 << OCIE0A);
     sei();
 
     //Activar pullups en las entradas
     pinGetLevel_init(); //with Changed=flag activated at initialization
 
-    
     //fast PWM only in OC1A,OC1B normal pin
     TCCR1A = (1 << COM1A1) | (0 << COM1A0) | (0 << COM1B1) | (0 << COM1B0) | (1 << WGM11) | (0 << WGM10);
     TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10);
     TCNT1 = 0;
     //
-    PWM_BIT_RESOL_SELECTED_latest = eeprom_read_byte(&PWM_BIT_RESOL_SELECTED_EEMEM_latest);
-    if ( !updateIfChange(&PWM_BIT_RESOL_SELECTED_latest))//if not change, then, load from EEPROM
+    
+    PWM_RESOL_SELECTED = get_pwm_bitresol();
+    if (!updatePWMRegsIfChange(PWM_RESOL_SELECTED))//if not change, then, load from EEPROM
     {
-        OCR1A = eeprom_read_word(&PWM);
-        ICR1 = eeprom_read_word(&ICR1_latest);
-        //
+        //Si no ha cambiado, entonces carga directamente de EEPROM al iniciar el programa
+        OCR1A = eeprom_read_word(&OCR1A_EEMEM);
+        ICR1 = eeprom_read_word(&ICR1_EEMEM);
+        ConfigOutputPin(DDRB, 1); //OC1A
+        
+        voltage_step = (float)(VOUT_MAX/((uint32_t)0x00001<<PWM_RESOL_SELECTED));
+        print_pwm_bit_resol(PWM_RESOL_SELECTED);
+        print_voltageStep(voltage_step);
     }
-    voltage_step = (float)(VOUT_MAX/((uint32_t)0x00001<<PWM_BIT_RESOL_SELECTED));
-   
-    ConfigOutputPin(DDRB, 1); //OC1A
+    
    
     while (1)
     {
-        
         if (isr_flag.sysTickMs)
         {
             isr_flag.sysTickMs = 0;
@@ -224,7 +226,8 @@ int main(void)
             {
                 counter0 = 0;
                 //
-                updateIfChange(&PWM_BIT_RESOL_SELECTED_latest);
+                PWM_RESOL_SELECTED = get_pwm_bitresol();
+                updatePWMRegsIfChange(PWM_RESOL_SELECTED);
                 //
                 pinGetLevel_job();
                 //-
@@ -244,7 +247,7 @@ int main(void)
                         counter1 = 0;
                         mainflag.clearSignPlusMinus = 1;
                         
-                        eeprom_update_word(&PWM, OCR1A);
+                        eeprom_update_word(&OCR1A_EEMEM, OCR1A);
 
                     }
                 }
@@ -265,7 +268,7 @@ int main(void)
                         counter1 = 0;
                         mainflag.clearSignPlusMinus = 1;
                         
-                        eeprom_update_word(&PWM, OCR1A);
+                        eeprom_update_word(&OCR1A_EEMEM, OCR1A);
                     }
                 }
                // 
